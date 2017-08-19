633db38faf504cf796a508a00fbc31704f7b0c8d
pooja165 <npoojasjce@gmail.com>
2017-08-18 22:58:43 -0700

    interactive quiz

---
+++ Web_based_Interactive_Quiz/question.js
@@ -0,0 +1,210 @@
+/* Name : Pooja Nanjundaswamy Student ID : W1177463 */
+
+var subjects_array = ["How much do you know about USA", "Science", "Geography"];
+
+var questions =[
+[
+{
+question: "In which year was the independence from England declared?",
+questionType: 2,
+correctAnswer: "1776",
+score: 10
+},
+{
+question: "What do Americans celebrate on 4th July?",
+questionType: 1,
+choices: ["Thankgiving day","The Discovery of America","Independence Day","The Revolution"],
+correctChoice: "Independence Day",
+score: 5
+},
+{
+question: "Where's the White House located?",
+questionType: 1,
+choices: ["New York","Washington, D.C","Chicago","Boston"],
+correctChoice: "Washington, D.C",
+score: 5
+},
+{
+question: "How many states are there in the US?",
+questionType: 2,
+correctAnswer: "50",
+score: 10
+},
+{
+question: "Which president freed the slaves?",
+questionType: 1,
+choices: ["Thomas Jefferson","George Washington","Abraham Lincoln","Bill Clinton"],
+correctChoice: "Abraham Lincoln",
+score: 5
+},
+{
+question: "What are the two major parties in the USA?",
+questionType: 1,
+choices: ["The Republics and the Socialists","The Conservationists and the Democrats","The Democrats and the Labour Party","The Republicans and the Democrats"],
+correctChoice: "The Republicans and the Democrats",
+score: 5
+},
+{
+question: "Who was the civil rights leader who fought through nonviolent action?",
+questionType: 1,
+choices: ["George Bush Jnr","Martin Luther King Jr","John Kennedy","Leon Trotsky"],
+correctChoice: "Martin Luther King Jr",
+score: 5
+},
+{
+question: "Who was the first person to walk on the moon?",
+questionType: 1,
+choices: ["John Glenn","Clint Eastwood","Neil Armstrong","Yuri Gagarin"],
+correctChoice: "Neil Armstrong",
+score: 5
+},
+{
+question: "What organization tries to find solutions to world problems and disputes?",
+questionType: 1,
+choices: ["The United Nations","IMF","FIFA","NATO"],
+correctChoice: "The United Nations",
+score: 5
+},
+{
+question: "Who said: 'We hold these truths to be self evident that all men are created equal'?",
+questionType: 1,
+choices: ["Bill Clinton","George Washington","Richard Nixon","Thomas Jefferson"],
+correctChoice: "Thomas Jefferson",
+score: 5
+}
+],
+
+[
+{
+question: "Which is the biggest planet in our solar system?",
+questionType: 2,
+correctAnswer: "Jupiter",
+score: 10
+},
+{
+question: "What is the chemical symbol for the element oxygen?",
+questionType: 1,
+choices: ["O","H","Ox","O2"],
+correctChoice: "O",
+score: 5
+},
+{
+question: "What is the name of the closest star to the earth?",
+questionType: 1,
+choices: ["Moon","Sun","Proxima Centauri","Sirius"],
+correctChoice: "Sun",
+score: 5
+},
+{
+question: "Pure water has a pH level of a around?",
+questionType: 2,
+correctAnswer: "7",
+score: 10
+},
+{
+question: "What famous scientist was awarded the 1921 Nobel Prize in Physics for his work on theoretical physics?",
+questionType: 1,
+choices: ["Newton","Galileo","Pascal","Einstein"],
+correctChoice: "Einstein",
+score: 5
+},
+{
+question: "How many bones do sharks have in their bodies?",
+questionType: 2,
+correctAnswer: "0",
+score: 10
+},
+{
+question: "Earth is located in which galaxy?",
+questionType: 1,
+choices: ["Milky Way","Whirlpool","Tadpole","Cigar"],
+correctChoice: "Milky Way",
+score: 5
+},
+{
+question: "The compound 'HCl' is ",
+questionType: 1,
+choices: ["An acid","A base","Neutral","Both acid and base"],
+correctChoice: "An acid",
+score: 5
+},
+{
+question: "True or false? Yogurt is produced by bacterial fermentation of milk.",
+questionType: 2,
+correctAnswer: "True",
+score: 10
+},
+{
+question: "True or false? The fastest land animal in the world is the zebra.",
+questionType: 2,
+correctAnswer: "False",
+score: 10
+}
+],
+[
+{
+question: "Which is the smallest country in the world?",
+questionType: 1,
+choices: ["Monaco","Nauru","Vatican city","Denmark"],
+correctChoice: "Vatican city",
+score: 5
+},
+{
+question: "Which is the largest US state by area?",
+questionType: 2,
+correctAnswer: "Alaska",
+score: 10
+},
+{
+question: "Which is the most populous country in the world?",
+questionType: 2,
+correctAnswer: "China",
+score: 10
+},
+{
+question: "What is the capital of Australia?",
+questionType: 1,
+choices: ["Sydney","Canberra","Melbourne","Adelaide"],
+correctChoice: "Canberra",
+score: 5
+},
+{
+question: "What is the capital of Spain?",
+questionType: 1,
+choices: ["Madrid","Seville","Barcelona","Malaga"],
+correctChoice: "Madrid",
+score: 5
+},
+{
+question: "What country is home to the tallest mountain in the world, Mount Everest?",
+questionType: 2,
+correctAnswer: "Nepal",
+score: 10
+},
+{
+question: "What is the capital of China?",
+questionType: 2,
+correctAnswer: "Beijing",
+score: 10
+},
+{
+question: "What is the capital of California?",
+questionType: 2,
+correctAnswer: "Sacramento",
+score: 10
+},
+{
+question: "Which is the longest river in the world?",
+questionType: 1,
+choices: ["Amazon River","Nile","Yellow River","Congo River"],
+correctChoice: "Nile",
+score: 5
+},
+{
+question: "True or False? Tokyo is the capital of Japan.",
+questionType: 2,
+correctAnswer: "True",
+score: 10
+}
+]
+];
---
+++ Web_based_Interactive_Quiz/readme.txt
@@ -0,0 +1,13 @@
+readme
+-----------
+This folder contains 3 files
+quiz_subject.html - containg the html and javascript quiz code
+styles.css - style specification for quiz_subject.html
+question.js - javascript file containing 3 categories and 10 questions(both type 1 and tye 2 questions) in each category.
+
+This was tested by placing it in a wamp server for use of cookies (to retain the same subject for the entire day)
+
+All the requirements mentioned are met by this application.
+
+Validation for html and css files are done and reports are attached.
+
