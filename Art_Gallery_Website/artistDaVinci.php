3a1d24bd71a96005db94480e58b812ba6fcb6456
pooja165 <npoojasjce@gmail.com>
2017-08-18 23:03:50 -0700

    art gallery website

---
+++ Art_Gallery_Website/artistDaVinci.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="style2.css" />
+    <script type="text/javascript" src="jquery-1.12.0.js"></script>
+    <title>Galerie d'Art</title>
+</head>
+<body>
+    <?php include("menu.php"); ?> 
+    
+    
+    <div id="paintinginfo" style="height:580px" class="col-12">
+        <p><center><b>Leonardo da Vinci</b></center></p>
+            <img src="daVinci.jpg" alt="davinci"style="width:300px;height:380px;border:10px solid black;padding:25px;position:relative;float:left;left:200px;top:20px;" class="image">
+              
+		      <div style="width:500px;position:relative;float:right;right:100px;padding:20px;top:20px;" class="grad">
+                <p><i><strong>Leonardo di ser Piero da Vinci</strong>, more commonly <strong>Leonardo da Vinci</strong>(15 April 1452 - 2 May 1519), was an Italian polymath whose areas of interest included invention, painting, sculpting, architecture, science, writing, history and cartography.</i></p>
+<p><i>He has been variously called the father of paleontology, ichnology, and architecture, and is widely considered one of the greatest painters of all time.</i></p>
+<p><i>Sometimes credited with the inventions of the parachute, helicopter and tank, his genius epitomized the Renaissance humanist ideal.</i></p>
+                </div>
+    
+    </div>
+    
+    
+    
+    
+  
+    <div id="footer">
+    <p><i><strong>&copy; Information uploaded by Galerie d'Art</strong></i></p>
+    </div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Art_Gallery_Website/readme.txt
@@ -0,0 +1,14 @@
+readme :
+
+Note on the following:
+Paintings:
+Comments and ratings are dynamically updated. AJAX implemented.
+
+Galleries Near you :
+a. Click to search art galleries nearby - Loads location map based on geolocation and shows the art galleries nearby.
+b. Search at a specific location - Type a location like San jose, sunnyvale etc and then click on "Click to search art galleries nearby". Loads the specific location map and shows the art galleries nearby.
+c. Keyword - Type in a partial name of art gallery like "Gallery". It shows all the galleries whose name contains gallery.
+
+Chat:
+To run chat, table needs to be created in sql database. 
+Table is given in inc folder of chat(inc folder of source inside the project folder)
